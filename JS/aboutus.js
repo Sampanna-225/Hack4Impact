25c916818f0deeccae7ca1defb260dd93feb7e96
Sampanna-225 <sampanna339@gmail.com>
2026-06-11 12:41:34 +0545

    update phase 2

--- JS/aboutus.js
+++ JS/aboutus.js
@@ -1,23 +1,89 @@
+//Hover button function,
 const buttons = document.querySelectorAll('.nav-btn');
 function Button_hover(){
     buttons.forEach(button => {
-            button.style.background = 'linear-gradient(90deg, #1093da, #5e2ce0, #9d00ff, #1093da)';
+            button.style.background = 'linear-gradient(90deg, #da1010, #e02c2c, #9d00ff, #1093da)';
             button.style.color = "#ffffff";
             button.style.border = "2px solid #e1e1e1";
             button.style.backgroundSize = '300% 100%';
             button.style.backgroundPosition = '0% 0%';
             button.style.transition = 'background-position 0.5s ease, transform 0.3s ease, box-shadow 0.3s ease';
-    });
-    button.onmouseenter = function() {
+    
+        button.onmouseenter = function() {
+            this.style.background = 'linear-gradient(90deg, #f30c0c, #ed1000, #ff0000, #0015ff)';
             this.style.backgroundPosition = '100% 0';
+            this.style.backgroundSize = '300% 100%';
             this.style.transform = 'translateY(-3px)';
             this.style.boxShadow =  '0 0 15px #9d00ff, 0 0 10px #5e2ce0';
         };
 
-    button.onmouseleave = function() {
-        this.style.backgroundPosition = '0% 0%';
-        this.style.transform = 'translateY(0)';
-        this.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
-    };
+        button.onmouseleave = function() {
+            this.style.background = 'linear-gradient(90deg, #f51e1e, #e02c2c, #9d00ff, #1093da)';
+            this.style.backgroundPosition = '0% 0%';
+            this.style.backgroundSize = '300% 100%';
+            this.style.transform = 'translateY(0)';
+            this.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+        };
+    });
 }
 Button_hover()
+
+//Enter animation:
+document.addEventListener("DOMContentLoaded", () => {
+    const mainNav = document.getElementById('Nav');
+    const buttonContainer = document.querySelector('.center-buttons');
+
+    if (mainNav) {
+        mainNav.addEventListener('animationend', (e) => {});
+    }
+
+    if (buttonContainer) {
+        buttonContainer.addEventListener('animationend', (e) => {});
+    }
+});
+
+//For Light on Screen:
+const nav = document.querySelector('.light');
+let fadeTimer; 
+let appearTime;
+let opav = 0
+function FadeOut(){
+    fadeTimer = setInterval(()=>{
+    opav -= 0.06 //Slowly decrease opacity.
+    if(opav <= 0){
+        nav.style.setProperty('--opacity', '0');
+        clearInterval(fadeTimer); //ends interval if opac is less or equal to 0.
+    }
+    else{
+        nav.style.setProperty('--opacity',opav) //The updated opacity.
+    }
+    },50);
+}
+nav.addEventListener('mousemove', (e) => {
+    //Clear the countdown timer if the mouse is still moving.
+    clearInterval(fadeTimer);
+    clearInterval(appearTime);
+
+    const rect = nav.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    //takes the pixel loaction of the pointer and converts it into white.
+    nav.style.setProperty('--x', `${x}px`);  
+    nav.style.setProperty('--y', `${y}px`);
+
+    appearTime = setInterval(()=>{ //Handles fade in
+        if (opav >= 0.8){
+            opav = 0.8
+            nav.style.setProperty('--opacity', opav);
+            clearInterval(appearTime);
+            FadeOut();//FadeOut is created a function so it affects the motion only if the intensity is max.
+
+        }
+        else if(opav < 0.8){
+            opav += 0.05
+            nav.style.setProperty('--opacity',opav);
+        }
+    },10);   
+    let oldx = x;
+});
